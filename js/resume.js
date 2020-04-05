f64bfabfb177e84623ec686d83d187f625a6376d
Leslie Antuche <leslieantuche@gmail.com>
2020-04-05 12:27:04 -0600

    Responsividad

--- js/resume.js
+++ js/resume.js
@@ -17,7 +17,7 @@
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function () {
-    $('.navbar-collapse').collapse('hide');
+    $('.navbar-collapse').collapse('hide')
   });
 
   // Activate scrollspy to add active class to navbar items on scroll
